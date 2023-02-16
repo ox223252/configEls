b9b7351fcf6e4b654fe6d01eee76736adcafb91f
Pierre Sauleau <psauleau@faureherman.com>
2023-02-16 15:25:39 +0100

    Add edit Els on config view if needded
    Modifications qui seront validées :
    	modifié :         Config.js

--- Config.js
+++ Config.js
@@ -1,5 +1,5 @@
 class Config {
-	constructor ( config, id )
+	constructor ( config, id, param = {} )
 	{
 		if ( ( undefined == config )
 			|| ( undefined == id ) )
@@ -37,6 +37,27 @@ class Config {
 				this._callbacksFunctions.push ( ...entry.callbacks );
 			}
 		}
+
+		if ( true == this._config.editable )
+		{
+			let index = this._config.dataset.length;
+			let domNew = document.createElement ( "div" );
+			domNew.innerHTML = '+';
+			domNew.style.display = "flex";
+			domNew.style.alignItems = "center";
+			domNew.style.justifyContent = "center";
+			domNew.style.fontSize = "3em";
+			domNew.style.cursor = "pointer";
+
+			domNew.onclick = param.editConfig.callback;
+			domNew.className = param.editConfig.class;
+
+			this._config.dataset[ index ] = {
+				el: domNew,
+				x:1,
+				y:1
+			}
+		}
 	}
 
 	get callbacks ( )
@@ -71,5 +92,37 @@ class Config {
 
 		socket.emit ( "list-clean" );
 		socket.emit ( "list-set", JSON.stringify(list) );
+	}
+
+	update ( )
+	{
+		for ( let [i,ds] of this._config.dataset.entries() )
+		{
+			if ( ( null != ds.el )
+				&& ( undefined != ds.el )
+				|| ( undefined == ds.data ) )
+			{
+				continue;
+			}
+
+			ds.el = document.createElement ( "div" );
+			ds.el.className = ds.class;
+			ds.el.style = ds.style;
+			ds.el.title = ds.name;
+
+			for ( let [j,da] of ds.data.entries() )
+			{
+				if ( !Els[ da.type ] )
+				{
+					continue;
+				}
+				let uId = this._id+"_"+i+"_"+j;
+				let entry = new Els[ da.type ]( da, uId );
+
+				ds.el.appendChild ( entry.dom );
+
+				this._callbacksFunctions.push ( ...entry.callbacks );
+			}
+		}
 	}
 }
